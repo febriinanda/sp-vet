179555d4c28d65aa5c4ac7f58f11d1a740966dc3
Istigfar Rahman <istigfar9237@gmail.com>
2021-10-20 22:35:41 +0700

    initial commit

---
+++ src/test/java/com/nanda/vet/VetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanda.vet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
